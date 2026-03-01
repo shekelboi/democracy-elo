346b6287e7b6a3226c8e9413d45ad912ffff4b46
Kevin <kevin005005@gmail.com>
2026-03-01 18:57:56 +0100

    Added ELO calculation

---
+++ main.py
@@ -0,0 +1,20 @@
+def calculate_elo(country1: float, country2: float, winner: bool):
+    """
+
+    :param country1: ELO rating of the first country.
+    :param country2: ELO rating of the second country.
+    :param winner: True if the first country won, False if the second country won.
+    :return: The change between the two countries' scores.
+    The result has to be added to the first country and subtracted from the second.
+    """
+    winner = int(winner)
+    diff = country2 - country1
+    diff /= 400
+    inverse_chance = 10 ** diff + 1
+    # Chance of country1 winning
+    expected_result = 1 / inverse_chance
+
+    # Calculating the change in ELO rating
+    k_factor = 20
+    change = k_factor * (winner - expected_result)
+    return change
